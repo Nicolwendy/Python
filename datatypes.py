891bfb5d141010dcb2721e1ff11c075726dbbd21
Nicolwendy <otienonicolwendy@gmail.com>
2025-11-06 12:32:13 +0300

    initial file

---
+++ datatypes.py
@@ -0,0 +1,16 @@
+jina="Emobilis"  #string
+nambari1="45" #int
+nambari2="40" #int
+mybool=True #boolean
+mylist=["mangoes","banana","apple"] #list
+mytuple=('jeep','jetour','mclaren') #tuple
+myset={'hp','lenovo','dell'} #set
+mydic={'Name':'Wendy','Age':'20','Gender':'Female'} #dictionary
+print(f"My school is {jina}")
+print(f"first number is {nambari1}")
+print(f"Second number is {nambari2} ")
+print(f"It is {mybool} that I study at Emobilis")
+print(f"I love the following fruits:{mylist}")
+print(mytuple)
+print(myset)
+print(mydic)
---
+++ operators.py
@@ -0,0 +1,32 @@
+#Arithmetic Operators
+x=7
+y=3
+print(x+y) #addition
+print(x-y) #subtraction
+print(x*y) #multiplication
+print(x/y) #division
+print(x%y) #modulus/remainder
+print(x ** y) #exponential
+print(x // y) #floor division
+
+#comparison operators
+num1=17
+num2=5
+print(num1 == num2) #equal to
+print(num1 != num2) #not equal to
+print(num1 > num2) #greater than
+print(num1 < num2) #less than
+print(num1 >= num2) #greater than or equal to
+print(num1 <= num2) #less than or equal to
+
+#logical operators
+num3=6
+print(num3 > 0 and num3 < 10)
+print(num3 > 0 or num3 < 10)
+print(0<num3<10)
+
+
+
+
+
+
