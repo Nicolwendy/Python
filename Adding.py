74f90ba20dbfb3a38dd30cb216f99780867d6ae6
Nicolwendy <otienonicolwendy@gmail.com>
2025-11-07 11:40:18 +0300

    new file

---
+++ Adding.py
@@ -0,0 +1,4 @@
+num1=int(input("Enter the first number"))
+num2=int(input("Enter the second number"))
+num3=int
+print(f"The sum of {num1} and {num2} is {num1+num2}")
---
+++ input output.py
@@ -0,0 +1,27 @@
+name=input("What's your name?")
+
+print(f"Hello,{name}")
+prename=input("How would you like me to call you?")
+
+age=int(input("Enter your age"))
+print(f"{prename},You will be {age+1} next year!!")
+problem=input(f"{prename}, is your issue?")
+print(f"Dear{prename},your issue will be solved shortly.")
+issue=input(f"{prename}, you have another issue??")
+num1="yes"
+num2="no"
+
+
+name = input("Enter your name: ")  # String Input
+age = int(input("Enter your age: ")) # Integer Input
+marks = float(input("Enter your marks: ")) # Float Input
+print("The name is:", name)
+print("The age is:", age)
+print("The marks is:", marks)
+
+
+
+
+
+
+
